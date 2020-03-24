4c4aa0be4c21ae431e2ebe82b691ffd2f653c32f
angellball <angell_ball@163.com>
2020-03-24 21:01:00 +0800

    Update srlink.js

--- Script/Crack/srlink.js
+++ Script/Crack/srlink.js
@@ -1,4 +1,10 @@
-#srlink vpn 无限VIP时长
+/*
+srlink vpn 无限VIP时长
+
+[mitm]
+hostname = abc.vpncenters.com
+*/
+
 var obj = JSON.parse($response.body);
 if ($request.url.match(/\/api\/user\/\.\*times=/g) != -1){
   obj.data["vipEndTime"] = "2099-03-25 19:08:50";
