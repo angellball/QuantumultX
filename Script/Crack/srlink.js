005a8eead155742ce96157fa37625a85e7aded0f
angellball <angell_ball@163.com>
2020-03-24 20:11:03 +0800

    Update srlink.js

--- Script/Crack/srlink.js
+++ Script/Crack/srlink.js
@@ -1,6 +1,6 @@
 var obj = JSON.parse($response.body);
 if ($request.url.match(/\/api\/user\/\.\*times=/g) != -1){
   obj.data["vipEndTime"] = "2099-03-25 19:08:50";
-  obj.data.["userType"] = "vip";
+  obj.data["userType"] = "vip";
 }
 $done({body: JSON.stringify(obj)});
